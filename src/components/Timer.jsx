dee4efc486405f2d0d8f4171f105fcd1ad9439f5
a5hcodes <rajana22cs@student.mes.ac.in>
2025-04-17 21:18:00 +0530

    neww

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,9 +3,17 @@ import React, { useEffect, useState } from 'react'
 const Timer = () => {
     const [timeLeft, setTimeLeft] = useState(25 * 60);
     const [isRunning, setIsRunning] = useState(false);
+    const [mode, setMode] = useState("focus");
 
     useEffect(() => {
         let timer;
+
+        // Set the timer when mode changes and the timer is NOT running
+        if (!isRunning) {
+            setTimeLeft(mode === "focus" ? 25 * 60 : 5 * 60);
+        }
+
+        // If running, start countdown
         if (isRunning) {
             timer = setInterval(() => {
                 setTimeLeft((prev) => {
@@ -18,7 +26,7 @@ const Timer = () => {
             }, 1000)
         }
         return () => clearInterval(timer);
-    }, [isRunning]);
+    }, [isRunning, mode]);
 
     const formatTimer = (seconds) => {
         const min = String(Math.floor(seconds / 60)).padStart(2, '0');
@@ -33,14 +41,16 @@ const Timer = () => {
 
     }
     return (
-
-        <div className='bg-pink-100'>
-            <div className='items-center justify-center '>
-                <h1 className='items-center font-bold text-pink-900 flex-col'>Plumoon - Pomodoro Timer</h1>
-                <p className='time'>{formatTimer(timeLeft)}</p>
-                <div className='flex flex-col'>
-                    <button onClick={() => setIsRunning(!isRunning)}>{isRunning ? "Pause" : "Start"}</button>
-                    <button onClick={resetTimer}>Reset</button>
+        <div className='flex items-center min-h-screen justify-center'>
+            <div className='flex flex-col gap-6'>
+                <h1 className='text-4xl font-bold text-pink-800 tracking-wider'>Plumoon - Pomodoro Timer</h1>
+                <p className='text-9xl font-extrabold tracking-widest m-2 p-3'>{formatTimer(timeLeft)}</p>
+                <p className='flex felx-col text-xl  justify-center'>{mode === "focus"? "Focus Mode" : "Break Mode"}</p>
+                <div className='flex gap-4 mt-4'>
+                    <button className='bg-pink-300 py-2 px-4 rounded-xl w-32 hover:bg-pink-900 hover:text-white transition duration-500 shadow-md' onClick={() => setIsRunning(!isRunning)}>{isRunning ? "Pause" : "Start"}</button>
+                    <button className='bg-pink-300 py-2 px-4 rounded-xl w-32 hover:bg-pink-900 hover:text-white transition duration-500 shadow-md' onClick={resetTimer}>Reset</button>
+                    <button className='bg-pink-300 py-2 px-4 rounded-xl w-32 hover:bg-pink-900 hover:text-white transition duration-500 shadow-md' onClick={() => { setMode("focus") }}> Focus</button>
+                    <button className='bg-pink-300 py-2 px-4 rounded-xl w-32 hover:bg-pink-900 hover:text-white transition duration-500 shadow-md' onClick={() => { setMode("break") }}>Break</button>
                 </div>
             </div>
         </div>
